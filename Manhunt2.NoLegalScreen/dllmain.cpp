ea19100d64c277436ceebfa8fb343ba511d7ce64
ermaccer <40604575+ermaccer@users.noreply.github.com>
2018-08-11 12:57:25 +0200

    Add files via upload

--- Manhunt2.NoLegalScreen/dllmain.cpp
+++ Manhunt2.NoLegalScreen/dllmain.cpp
@@ -1,12 +1,13 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "..\shared\stdafx.h"
-#include "..\shared\CPatch.h"
+#include "..\shared\MemoryMgr.h"
 
+using namespace Memory::VP;
 void Init()
 {
-	CPatch::SetInt(0x53FC68,0);
-	CPatch::SetInt(0x53FC50,0);
-	CPatch::SetInt(0x53FC6F,0);
+	Patch(0x53FC68, 0);
+	Patch(0x53FC50, 0);
+	Patch(0x53FC6F, 0);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
